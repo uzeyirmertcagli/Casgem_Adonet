076df3bc8d713b83aa7ad71a34891f34dd55355f
Mert Çağlı <mertcagli00@gmail.com>
2023-07-26 16:18:00 +0300

    Add project files.

---
+++ Casgem_Adonet/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Casgem_Adonet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection("Data Source=LAPTOP-1DJS186I;Initial Catalog=CasgemDbMovie;Integrated Security=True");
+
+        private void btnCategoryList_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select * From TblCategory" , connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable da = new DataTable();
+            adapter.Fill(da);
+            dtgCategory.DataSource = da;
+
+        }
+
+        private void btnCategoryAdd_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("insert into TblCategory (CategoryName) values (@p1)", connection);
+            command.Parameters.AddWithValue("@p1", txtCategoryName.Text);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Kategori Başarılı Bir Şekilde Kaydedildi!","Bilgi", MessageBoxButtons.OK , MessageBoxIcon.Information);
+            connection.Close();
+        }
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("Delete From TblCategory where CategoryID=@p1", connection);
+            command.Parameters.AddWithValue("@p1" , txtCategoryID.Text);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Kategori Başarılı Bir Şekilde Silindi!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            connection.Close();
+        }
+    }
+}
